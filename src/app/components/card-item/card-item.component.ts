d5a10808b0db095ae1c0baa7084c510432847453
Anna <anna4776497@gmail.com>
2023-04-16 23:09:57 +0300

    storage is exist

--- src/app/components/card-item/card-item.component.ts
+++ src/app/components/card-item/card-item.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core'
 import { IBeerItem } from 'src/app/interfaces/interfaces'
+import { Preferences } from '@capacitor/preferences';
 
 
 @Component({
-    selector: 'card-item', 
+    selector: 'card-item',
     templateUrl: './card-item.component.html',
     styleUrls: ['./card-item.component.css']
 })
 
 export class CardItemComponent {
-    @Input() item: IBeerItem    
+    @Input() item: IBeerItem
+    
 }
--- src/app/components/modal-item/modal-item.component.ts
+++ src/app/components/modal-item/modal-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { IBeerItem, ICheckedItem } from 'src/app/interfaces/interfaces';
 import { Preferences } from '@capacitor/preferences';
 
@@ -7,21 +7,41 @@ import { Preferences } from '@capacitor/preferences';
   templateUrl: './modal-item.component.html',
   styleUrls: ['./modal-item.component.css']
 })
-export class ModalItemComponent {
+export class ModalItemComponent implements OnInit, OnChanges{
   @Input() checkedItemIsOpen: boolean
   @Input() checkedItem: IBeerItem 
 
   @Output() buttonClick = new EventEmitter()
 
+  selected: boolean = false
+  disabled: boolean = false
+
   public unCheckedItem(): void {
     	this.buttonClick.emit();
   }
+
+  getSelectedBeer = async () => {
+    const item: string | any = await Preferences.get({ key: String(this.checkedItem.id) })
+    if (item.value) {
+      this.selected=true
+    } else {
+      this.selected=false
+    }
+  }
+    
   
   setBeerItem = async (key: number, value: IBeerItem) => { 
-    console.log(value)
     await Preferences.set({
       key: String(key),
       value: JSON.stringify(value),
     });
+    this.disabled = true;
+  }
+
+  ngOnInit(): void {
+
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.getSelectedBeer()
   }
 }
